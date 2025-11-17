c1cd031c5ff8bca61d91fb4e8eadeea1fa6e0671
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-11-17 10:29:46 +0600

    27-11 Add custom property to Request Parameter of Express, req.user

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -55,8 +55,10 @@ const createUser =  catchAsysnc(async(req: Request, res: Response, next: NextFun
 })
 const updateUser =  catchAsysnc(async(req: Request, res: Response, next: NextFunction)=> {
     const userId = req.params.id;
-    const token = req.headers.authorization;
-    const verifiedToken = verifyToken(token as string , envVars.JWT_ACCESS_SECRET) as JwtPayload
+    // const token = req.headers.authorization;
+    // const verifiedToken = verifyToken(token as string , envVars.JWT_ACCESS_SECRET) as JwtPayload
+    const verifiedToken = req.user
+
     const payload = req.body
     const user = await UserServices.updateUser(userId, payload, verifiedToken) 
 
