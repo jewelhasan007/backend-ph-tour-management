56a58bc50db2af237bfb2d40ef210407fcf8c963
Jewel Hasan <jewel.hasan@cg-bd.com>
2026-04-02 10:30:27 +0600

    29-9 Create interface and model for Division and interface for Tour

---
+++ src/app/modules/tour/tour.model.ts
@@ -0,0 +1,8 @@
+import { Schema } from "mongoose";
+import { ITour } from "./tour.interface";
+
+const tourSchema = new Schema<ITour>({
+title : {type: String}
+}, {
+    timestamps: true
+})
--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -1,9 +1,13 @@
 import { NextFunction, Request, Response } from "express";
+import { envVars } from "../config/env";
 
 type AsyncHandler = (req: Request, res: Response, next:NextFunction) => Promise<void>
 export const catchAsysnc = (fn: AsyncHandler) => (req: Request, res: Response, next:NextFunction) =>{
     Promise.resolve(fn(req, res, next)).catch((err: any) =>{
-        console.log(err);
+        if(envVars.NODE_ENV === "development"){
+      console.log(err);
+        }
+  
         next(err)
     })
 }
